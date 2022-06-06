8173de4a1f02eb695f1c9b9d0a01ae5af8c04008
Ad1th1 <adithibhatindia@gmail.com>
2022-06-06 11:36:04 +0530

    Update and rename handwriting_recognition.md to handwriting_recognition.py

--- Federated Learning/Task 1 -> CNN/handwriting_recognition.py
+++ Federated Learning/Task 1 -> CNN/handwriting_recognition.py
@@ -1,37 +1,8 @@
-Handwriting Recognition:
+# Handwriting Recognition:
 
-Source : https://www.youtube.com/watch?v=XF0q-uLxM7s
+# Source : https://www.youtube.com/watch?v=XF0q-uLxM7s
 
-  Pattern recognition is awesome
-  
-  Softwares Used
-  1. Tensorflow
-  2. Keras
-  3. Numpy
-  4. Matplotlib
-  5. Jupyter Notebook
-  
-  MNIST Data set
-  -> training set of 60,000 examples
-  -> test-set of 10,000 examples
-  -> 784 features per image
-  -> each image is labelled with respective digits
-  
-  data = training(builds the system) + test set(check the correctness of the system)
-  
-  Training set
-  -> used to build a model
-  -> consists of a set of images used to train the system
-  -> training rules and algos used give relevant info on how to associate input data with output decision
-  -> system is trained by applying these algos on the dataset
-  -> usually comprises 80% of the data
-  
-  Test set
-  -> used to check whether system is producing correct output after training or not.
-  -> usually comprises 20% of the data
-  
-  Jupyter Notebook
-  -> powerful tool to present data-science projects
+
 
 import tensorflow as tf
 import matplotlib.pyplot as plt
